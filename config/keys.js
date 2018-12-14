32294dae6ab84bd6ba3a7e7e28810312cf1c8054
davidtsai130 <davidtsai130@gmail.com>
2018-12-13 19:46:31 -0500

    some validations

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://david:password1@ds143141.mlab.com:43141/friendconnector"
+  mongoURI: 'mongodb://david:password1@ds143141.mlab.com:43141/friendconnector',
+  secretOrKey: 'secret'
 };
